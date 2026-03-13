8768c93b67de980249763aef767906ce19477470
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-03-13 17:52:24 +0530

    Add files via upload

---
+++ truple_operations.py
@@ -0,0 +1,18 @@
+truplex = ("truple", False, 3.2, 1)
+print(truplex)
+
+truplex = (4,6,2,8,3,1)
+print(truplex)
+
+truplex = truplex = (9,)
+print(truplex)
+
+truple1 = (50,40,10,60,70)
+print(truple1.count(50))
+
+truplex = (2,4,3,5,4,6,7,8,6,1)
+_slice = truplex[3:5]
+print(_slice)
+
+_slice = truplex[:6]
+print(_slice)
